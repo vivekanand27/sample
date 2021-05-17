8667fa096207c7ad93fa74616ff15f2b56ef7e25
Nitish K <creativenitish@hotmail.com>
2021-05-17 17:11:02 +0530

    Modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import {Button, Row, Col} from 'react-bootstrap'
+import {useState} from 'react'
+import {Button, Row, Col, Modal} from 'react-bootstrap'
 
 function App() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
  const openPopup = () => {
 
@@ -13,13 +18,31 @@ function App() {
 
   return (
     <div className="App">
-      <Row className="py-10">
+      {/* <Row className="py-4">
         <Col xs={4}></Col>
         <Col xs={4}> <Button  variant="success">Success</Button>
         
         </Col>
         <Col xs={4}></Col>
-      </Row>
+      </Row> */}
+      <Button variant="primary" onClick={handleShow} className="modal-button">
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       
     </div>
   );
